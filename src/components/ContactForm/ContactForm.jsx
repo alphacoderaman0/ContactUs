bc501ba951c0043a0b6056b4b4cdb38f005df3ba
alphacoderaman0 <amanmittal0004g@gmail.com>
2024-05-06 18:52:22 +0530

    more changes

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -12,10 +12,27 @@ const ContactForm = () => {
           <Button text="VIA CALL"  icon={<IoIosCall />}/>
           </div>
           <Button isOutline={true} text="VIA EMAIL FORM"  icon={<MdOutlineMessage />}/>
-
-        
+          <form>
+            <div className={styles.formController}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name='name' />
+            </div>
+            <div className={styles.formController}>
+            <label htmlFor="email">E-Mail</label>
+            <input type="text" name='email' />
+            </div>
+            <div className={styles.formController}>
+            <label htmlFor="text">Text</label>
+            <textarea rows="7" type="text" name='text' />
+            </div>
+            <div style={{display:'flex',justifyContent:'end',marginRight:'2.5%'}}>
+            <Button text="SUBMIT"/>
+            </div>
+          </form>
         </div>
-        <div className={styles.contactImage}></div>
+        <div className={styles.contactImage}>
+          <img src="" alt="ContactImage" />
+        </div>
     </section>
   )
 }
